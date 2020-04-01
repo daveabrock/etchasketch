9539295c1540d249040ecc7fded1587cef4fb035
Brock, Dave A <Dave.Brock@cunamutual.com>
2020-04-01 13:15:38 -0500

    first push

---
+++ etch-a-sketch.js
@@ -0,0 +1,70 @@
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakeButton = document.querySelector('.shake');
+const MOVE_AMOUNT = 20;
+
+const { width, height } = canvas;
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+console.log(width, height);
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`
+
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+function clearCanvas() {
+    canvas.classList.add('shake');
+    ctx.clearRect(0, 0, width, height);
+    canvas.addEventListener('animationend', function() {
+        canvas.classList.remove('shake');
+    }, { once: true })
+}
+
+function draw({ key }) {
+    hue += 10;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+    console.log(key);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+            break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        default:    
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+function handleKey(e) {
+    if (e.key.includes('Arrow')) {
+        e.preventDefault();
+        draw({ key: e.key })
+    }
+    if (e.key === 'e') {
+        clearCanvas();
+    }
+}
+
+window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
